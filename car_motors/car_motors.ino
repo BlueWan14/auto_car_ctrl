0e23e2acfdceaa810f4249aae81f613648a01833
ISTY <erwanmawart@gmail.com>
2024-03-29 11:20:06 +0100

    Resources management between arduino and RasPi

--- car_motors/car_motors.ino
+++ car_motors/car_motors.ino
@@ -3,19 +3,19 @@
 #include <Servo.h>
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Bool.h>
 #include <auto_car_ctrl/motors.h>
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+// #include <Wire.h>
+// #include <LiquidCrystal_I2C.h>
 
 
 // PINS ================================================================================
-// Servo moteurs
+#define pin_cod 2
+#define Echo 4
+#define Trigger 5
 #define servo_vel 6
 #define servo_dir 7
-#define pin_dir 8
-#define pin_speed 9
-#define pin_SW 10
-#define pin_Batt A0
+// #define pin_Batt A0
 
 // BORNES DE COMMANDES =================================================================
 // Bornes liées à la vitesse
@@ -23,22 +23,11 @@
 #define NEUTRAL 1500
 #define BackwardMax 1000
 // Bornes liées à la direction de la voiture
-#define MIDDLE 90
-// Bornes liées à la vitesse
-#define ctrl_forward_max 1775
-#define ctrl_neutral 1360
-#define ctrl_backward_max 905
-// Bornes liées au braquage de la voiture
-#define ctrl_left_max 1530
-#define ctrl_middle 1306
-#define ctrl_right_max 1078
-// Spécifications télécommandes
-#define auto_limit 1800
-#define treshold_ctrller 30
-// Valeurs de tension batterie
-#define Tension_Max_Batterie 8.2
-#define Voltage_Alert_Batt 6.5
-#define Tension_Min_Batterie 6.0
+#define MIDDLE 96
+// // Valeurs de tension batterie
+// #define Tension_Max_Batterie 8.2
+// #define Voltage_Alert_Batt 6.5
+// #define Tension_Min_Batterie 6.0
 
 
 // VARIABLES ===========================================================================
@@ -46,50 +35,41 @@
 Servo MotorLinear;
 Servo MotorAngular;
 // Bornes liées à la direction de la voiture
-int TurnLeftMax = 115,
-      TurnRightMax = 65;
+int TurnLeftMax = MIDDLE + 40,
+    TurnRightMax = MIDDLE - 40;
 // Initialisation des variables de commande
 int MotSpeed = NEUTRAL,
     MotAngle = MIDDLE,
     Speed_mem = NEUTRAL;
-// Initialisation des variables de télécommande
-int SW = 0,
-    average_speed = ctrl_neutral,
-    average_dir = ctrl_middle,
-    i_speed = 1,
-    i_dir = 1;
-// Initialisation des variables de mesure de tension
-int Pourcentage_Batt;
-unsigned long MillisMem;
-float Tension;
-LiquidCrystal_I2C lcd(0x27,16,2);
+// // Initialisation des variables de mesure de tension
+// int Pourcentage_Batt;
+// unsigned long MillisMem;
+// float Tension;
+// LiquidCrystal_I2C lcd(0x27,16,2);
 
 
-// DÉFINITIONS DE FONCTIONS ============================================================
 /*
 description : Fonction callback appelé à chaque modification du topic auto_car/mot/cmd_vel
 paramètre : (const, geometry_msgs::Twist, pointeur) msg : message reçu.
 */
 void cmdCallback(const geometry_msgs::Twist &msg) {
-  if(SW > auto_limit) {
-    // Lecture des commandes selon le torseur envoyé -------------------------------------
-    float RosSpeedX = msg.linear.x;
-    float RosAngleZ = msg.angular.z;
-    
-    // Calcul des commandes réelles ------------------------------------------------------
-    MotSpeed = PtV(RosSpeedX, ForwardMax, NEUTRAL, BackwardMax);
-    MotAngle = RosAngleZ + MIDDLE;
+  // Lecture des commandes selon le torseur envoyé -------------------------------------
+  float RosSpeedX = msg.linear.x;
+  float RosAngleZ = msg.angular.z;
+  
+  // Calcul des commandes réelles ------------------------------------------------------
+  MotSpeed = PtV(RosSpeedX, ForwardMax, NEUTRAL, BackwardMax);
+  MotAngle = RosAngleZ + MIDDLE;
 
-    // Les variables sont contraintes sur leur plage de fonctionnement, elle ne peuvent pas en sortir
-    MotAngle = constrain(MotAngle, TurnRightMax, TurnLeftMax);
+  // Les variables sont contraintes sur leur plage de fonctionnement, elle ne peuvent pas en sortir
+  MotAngle = constrain(MotAngle, TurnRightMax, TurnLeftMax);
 
-    // Pour passer en vitesse négative, il faut d'abord "freiner" en engageant une première marche arrière
-    break_sys();
+  // Pour passer en vitesse négative, il faut d'abord "freiner" en engageant une première marche arrière
+  break_sys();
 
-    // Écriture de la nouvelle commande --------------------------------------------------
-    MotorLinear.writeMicroseconds(MotSpeed);
-    MotorAngular.write(MotAngle);
-  }
+  // Écriture de la nouvelle commande --------------------------------------------------
+  MotorLinear.writeMicroseconds(MotSpeed);
+  MotorAngular.write(MotAngle);
 }
 
 /*
@@ -147,22 +127,6 @@ void break_sys() {
   return;
 }
 
-/*
-description : lit et traite les informations envoyées par la télécommande
-paramètre : (int) pin : pin recevant les données
-            (int) defaultValue : valeur neutre de la donnée
-return : (int) Commande.
-*/
-int readController(int pin, int defaultValue, int* average, int* i) {
-  int cmd = pulseIn(pin, HIGH);
-
-  if(((cmd < (defaultValue - treshold_ctrller)) || (cmd > (defaultValue + treshold_ctrller))) && (cmd != 0)) {
-    return cmd;
-  } else {
-    return defaultValue;
-  }
-}
-
 
 // COMMUNICATION ROS ===================================================================
 // Initialisation des variables liées à ROS
@@ -170,19 +134,20 @@ ros::NodeHandle nh;
 auto_car_ctrl::motors msg_mot;
 // Initialisation d'export de donnée sur le topic "auto_car/mot/vel"
 ros::Publisher return_vel("auto_car/arduino/mot", &msg_mot);
-// Initialisation de reception de donnée sur le topic "auto_car/cmd_vel"
-ros::Subscriber<geometry_msgs::Twist> cmd_vel("auto_car/cmd_vel", &cmdCallback);
+// Initialisation de reception de donnée sur le topic "auto_car/arduino/cmd_vel"
+ros::Subscriber<geometry_msgs::Twist> cmd_vel("auto_car/arduino/cmd_vel", &cmdCallback);
 
 // SETUP ===============================================================================
 void setup() {
   // Définition des pins ---------------------------------------------------------------
-  pinMode(pin_speed, INPUT);
-  pinMode(pin_dir, INPUT);
-  pinMode(pin_SW, INPUT);
+  pinMode(pin_cod, INPUT);
+  pinMode(Trigger, OUTPUT);
+  pinMode(Echo, INPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
 
-  // Initialisation du lcd -------------------------------------------------------------
-  lcd.init();                      
-  lcd.backlight();
+  // // Initialisation du lcd -------------------------------------------------------------
+  // lcd.init();                      
+  // lcd.backlight();
 
   // Association des pins avec les servomoteurs ----------------------------------------
   MotorLinear.attach(servo_vel);
@@ -193,58 +158,52 @@ void setup() {
   nh.subscribe(cmd_vel);      // On s'abonne à cmd_vel
   nh.advertise(return_vel);   // On configure return_vel afin de publier dessus
 
+  // Initialisation des moteurs --------------------------------------------------------
+  MotorLinear.writeMicroseconds(NEUTRAL);
+  MotorAngular.write(MIDDLE);
+
+  while (!nh.connected())
+    nh.spinOnce();
   if(nh.getParam("/angle_max_left", &TurnLeftMax))
     TurnLeftMax = TurnLeftMax + MIDDLE;
   if(nh.getParam("/angle_max_right", &TurnRightMax))
     TurnRightMax = TurnRightMax + MIDDLE;
 
-  // Initialisation des moteurs --------------------------------------------------------
-  MotorLinear.writeMicroseconds(NEUTRAL);
-  MotorAngular.write(MIDDLE);
-
-  MillisMem = millis();
+  // MillisMem = millis();
 }
 
 // MAIN ================================================================================
 void loop() {
-  // Réception des données de la télécommande ------------------------------------------
-  SW = pulseIn(pin_SW, HIGH); // Commande auto/manuel
-
-  if(SW <= auto_limit) {
-    msg_mot.RC_cmd.auto_select = false;
-    msg_mot.RC_cmd.speed = readController(pin_speed, ctrl_neutral, &average_speed, &i_speed),
-    msg_mot.RC_cmd.direction = readController(pin_dir, ctrl_middle, &average_dir, &i_dir);
-    
-    // Calcul des commandes réelles
-    MotSpeed = constrain(PtV(VtP(msg_mot.RC_cmd.speed, ctrl_forward_max+treshold_ctrller, ctrl_neutral, ctrl_backward_max-treshold_ctrller), ForwardMax, NEUTRAL, BackwardMax), BackwardMax, ForwardMax);
-    MotAngle = constrain(PtV(VtP(msg_mot.RC_cmd.direction, ctrl_left_max+treshold_ctrller, ctrl_middle, ctrl_right_max-treshold_ctrller), TurnLeftMax, MIDDLE, TurnRightMax), TurnRightMax, TurnLeftMax);
-
-    // Pour passer en vitesse négative, il faut d'abord "freiner" en engageant une première marche arrière
-    break_sys();
-
-    // Écriture de la nouvelle commande
-    MotorLinear.writeMicroseconds(MotSpeed);
-    MotorAngular.write(MotAngle);
+  if(pulseIn(pin_cod, CHANGE) < 1.0) {            // Valeur renvoyé par le codeur
+    msg_mot.coder = true;
   } else {
-    msg_mot.RC_cmd.auto_select = true;
+    msg_mot.coder = false;
   }
 
-  if((millis() - MillisMem) > 1000) {
-    // Calcul et affichage de la tension batterie ----------------------------------------
-    Tension = analogRead(pin_Batt) * 4.8 / 1023 * 2;
-    Tension = constrain(Tension, Tension_Min_Batterie, Tension_Max_Batterie);
-    Pourcentage_Batt = (Tension - Tension_Min_Batterie) * 100 / (Tension_Max_Batterie - Tension_Min_Batterie);
+  // if((millis() - MillisMem) > 1000) {
+  //   // Calcul et affichage de la tension batterie ----------------------------------------
+  //   Tension = analogRead(pin_Batt) * 4.8 / 1023 * 2;
+  //   Tension = constrain(Tension, Tension_Min_Batterie, Tension_Max_Batterie);
+  //   Pourcentage_Batt = (Tension - Tension_Min_Batterie) * 100 / (Tension_Max_Batterie - Tension_Min_Batterie);
 
-    // Affichage de la valeur sur le lcd -------------------------------------------------
-    lcd.clear();
-    lcd.setCursor(0,0);  
-    lcd.print("Batterie = " + String(Pourcentage_Batt) + "%");
-    if (Tension < Voltage_Alert_Batt) {
-      lcd.setCursor(6,1);
-      lcd.print("HELP");
-    }
-    msg_mot.voltage = Tension;
-  }
+  //   // Affichage de la valeur sur le lcd -------------------------------------------------
+  //   lcd.clear();
+  //   lcd.setCursor(0,0);  
+  //   lcd.print("Batterie = " + String(Pourcentage_Batt) + "%");
+  //   if (Tension < Voltage_Alert_Batt) {
+  //     lcd.setCursor(6,1);
+  //     lcd.print("HELP");
+  //   }
+  //   msg_mot.voltage = Tension;
+  // }
+
+  // Mesure ultra-sons ----------------------------------------------------------------------
+  digitalWrite(Trigger, LOW); // On efface l'etat logique de TRIG
+  delayMicroseconds(2);
+  digitalWrite(Trigger, HIGH); // On met la broche TRIG a "1" pendant 10µS
+  delayMicroseconds(10);
+  digitalWrite(Trigger, LOW); // On remet la broche TRIG a "0"
+  msg_mot.rearObstacle = pulseIn(Echo, HIGH) * 0.034 / 2; // Calcul de la distance : Durée totale * La vitesse du son (0.034 cm/µs) / 2 (Aller-retour des ultrasons)
 
   // Écriture de la vitesse et la rotation actuelle sur return_vel ---------------------
   msg_mot.vel.linear.x = VtP(MotSpeed, ForwardMax, NEUTRAL, BackwardMax);
@@ -253,5 +212,5 @@ void loop() {
 
   // Lecture de la communication avec ROS ----------------------------------------------
   nh.spinOnce();
-  delay(115);
+  delay(200);
 }
--- src/cmd_vel_node.cpp
+++ src/cmd_vel_node.cpp
@@ -14,16 +14,18 @@ float max_range, too_close,
       way = 1.0;
 int angle_max_left = 40,
     angle_max_right = -40;
+bool start;
 
 // Other sub and publisher ---------------------------------------------------------------------------------------
 ros::Subscriber processed_sub;
 ros::Subscriber way_sub;
+ros::Subscriber start_sub;
 ros::Publisher cmd_pub;
 
 // PROTOTYPES ====================================================================================================
 void lidarCallBack(const auto_car_ctrl::rosFloat &);
 void wayCallBack(const auto_car_ctrl::rosFloat &);
-
+void startCallBack(const auto_car_ctrl::rosBool &);
 
 // MAIN ==========================================================================================================
 int main(int argc, char** argv) {
@@ -42,6 +44,7 @@ int main(int argc, char** argv) {
     // Création des subscribers ----------------------------------------------------------------------------------
     processed_sub = nh.subscribe("/auto_car/cmd/lidar_process", 100, &lidarCallBack);
     way_sub = nh.subscribe("/auto_car/cmd/way", 100, &wayCallBack);
+    start_sub = nh.subscribe("/start", 100, &startCallBack);
 
     // Création des publishers -----------------------------------------------------------------------------------
     cmd_pub = nh.advertise<geometry_msgs::Twist>("auto_car/arduino/cmd_vel", 100);
@@ -60,23 +63,33 @@ paramètre : (const, auto_car_ctrl::rosFloat::ConstPtr, pointeur) angle_msg : me
 void lidarCallBack(const auto_car_ctrl::rosFloat &angle_msg) {
     geometry_msgs::Twist cmd;
 
-    // Commande de rotation des roues (en °)
-    cmd.angular.z = angle_msg.val * 180 / pi;                                   // Suivi de l'objectif
+    if(start) {
+        // Commande de rotation des roues (en °)
+        cmd.angular.z = angle_msg.val * 180 / pi;                                   // Suivi de l'objectif
 
-    // Commande de vitesse du moteur (en %)
-    if(cmd.angular.z < angle_max_right) {
-        // On ralenti pour tourner plus vite à droite
-        cmd.linear.x = speed_max - ((cmd.angular.z - angle_max_right) * speed_max / (-102.5 - angle_max_right));
-    } else if(cmd.angular.z > angle_max_left) {
-        // On ralenti pour tourner plus vite à gauche
-        cmd.linear.x = speed_max - ((cmd.angular.z - angle_max_left) * speed_max / (102.5 - angle_max_left));
+        // Commande de vitesse du moteur (en %)
+        if(cmd.angular.z < angle_max_right) {
+            // On ralenti pour tourner plus vite à droite
+            cmd.linear.x = speed_max - ((cmd.angular.z - angle_max_right) * speed_max / (-102.5 - angle_max_right));
+        } else if(cmd.angular.z > angle_max_left) {
+            // On ralenti pour tourner plus vite à gauche
+            cmd.linear.x = speed_max - ((cmd.angular.z - angle_max_left) * speed_max / (102.5 - angle_max_left));
+        } else {
+            cmd.linear.x = way * speed_max;                                         // On met les gaz
+        }
     } else {
-        cmd.linear.x = way * speed_max;                                         // On met les gaz
+        cmd.linear.x = 0;
+        cmd.angular.z = 0;
     }
-
+    
     cmd_pub.publish(cmd);
 }
 
 void wayCallBack(const auto_car_ctrl::rosFloat &way_msg) {
     way = way_msg.val;
+}
+
+
+void startCallBack(const auto_car_ctrl::rosBool &start_msg) {
+    start = start_msg.answer;
 }
